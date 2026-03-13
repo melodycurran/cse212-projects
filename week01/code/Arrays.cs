611b531a6df85d9a0f6cdab42024be9c627516d1
melodycurran <97179039+melodycurran@users.noreply.github.com>
2026-03-13 13:19:56 -0600

    Added the function

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -21,23 +21,27 @@ public static class Arrays
 		// Remember: Using comments in your program, write down your process for solving this problem
 		// step by step before you write the code. The plan should be clear enough that it could
 		// be implemented by another person.
+
+		//My solutions
 		//First I will initiate a variable that holds a new array. I will  name the variable multiplesArray
-		//Next, I will start my For
+		//Next, I will start my for loop that will run as long as i is less than the length. I will start i at 0 and increment it by 1 each time the loop runs.
+		//Inside the loop, I will calculate the possible multiples by multiplying i and number.
+		//Then I will calculate the modulo by taking the possible multiples and finding the remainder when divided by 3.
+		//If the modulo is equal to 0, then I will add the possible multiples to the multiplesArray.
+		//After the loop, I will convert the multiplesArray to an array and return it
 
-		List<double> newArray = new();
-
+		List<double> multiplesArray = new();
 		for (int i = 0; i < length; i++)
 		{
-			double possibleMultiples = i * number;
-			double modulo = possibleMultiples % 3;
-
-			if (modulo == 0) newArray.Add(possibleMultiples);
+			double possibleMultiple = number * (i + 1);
+			double modulo = possibleMultiple % 3;
+			if (modulo == 0)
+			{
+				multiplesArray.Add(possibleMultiple);
+			}
 		}
-		;
 
-		double[] array = newArray.ToArray();
-
-		return array; // replace this return statement with your own
+		return multiplesArray.ToArray(); // replace this return statement with your own
 	}
 
 	/// <summary>
@@ -53,5 +57,21 @@ public static class Arrays
 		// Remember: Using comments in your program, write down your process for solving this problem
 		// step by step before you write the code. The plan should be clear enough that it could
 		// be implemented by another person.
+
+		//My solution
+		//First I will create a new list called rotatedList that will hold the rotated values.
+		//Next, I will start a for loop that will run from 0 to the amount. Inside the loop, I will add the last element of the data list to the rotatedList and then remove it from the data list.
+		//After the loop, I will add the remaining elements of the data list to the rotatedList.
+		//Finally, I will clear the original data list and add the elements from the rotatedList back to the data list.
+		List<int> rotatedList = new();
+
+		for (int i = 0; i < amount; i++)
+		{
+			rotatedList.Add(data[data.Count - 1]);
+			data.RemoveAt(data.Count - 1);
+		}
+
+		data.AddRange(rotatedList);
+
 	}
 }
