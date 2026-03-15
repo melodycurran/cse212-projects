9a506d15fc075cf94964331f7baffd1a0557d28f
melodycurran <97179039+melodycurran@users.noreply.github.com>
2026-03-15 11:21:04 -0600

    Added code for Week 2 Queue

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,63 +1,68 @@
 ﻿public class PriorityQueue
 {
-    private List<PriorityItem> _queue = new();
+	private List<PriorityItem> _queue = new();
 
-    /// <summary>
-    /// Add a new value to the queue with an associated priority.  The
-    /// node is always added to the back of the queue regardless of 
-    /// the priority.
-    /// </summary>
-    /// <param name="value">The value</param>
-    /// <param name="priority">The priority</param>
-    public void Enqueue(string value, int priority)
-    {
-        var newNode = new PriorityItem(value, priority);
-        _queue.Add(newNode);
-    }
+	/// <summary>
+	/// Add a new value to the queue with an associated priority.  The
+	/// node is always added to the back of the queue regardless of 
+	/// the priority.
+	/// </summary>
+	/// <param name="value">The value</param>
+	/// <param name="priority">The priority</param>
+	public void Enqueue(string value, int priority)
+	{
+		var newNode = new PriorityItem(value, priority);
+		_queue.Add(newNode);
+	}
 
-    public string Dequeue()
-    {
-        if (_queue.Count == 0) // Verify the queue is not empty
-        {
-            throw new InvalidOperationException("The queue is empty.");
-        }
+	public string Dequeue()
+	{
+		if (_queue.Count == 0) // Verify the queue is not empty
+		{
+			throw new InvalidOperationException("The queue is empty.");
+		}
 
-        // Find the index of the item with the highest priority to remove
-        var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
-        {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
-                highPriorityIndex = index;
-        }
+		// Find the index of the item with the highest priority to remove
+		var highPriorityIndex = 0;
+		for (int index = 1; index < _queue.Count; index++)
+		{
+			if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
+			{ highPriorityIndex = index; }
 
-        // Remove and return the item with the highest priority
-        var value = _queue[highPriorityIndex].Value;
-        return value;
-    }
+		}
 
-    // DO NOT MODIFY THE CODE IN THIS METHOD
-    // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
-    public override string ToString()
-    {
-        return $"[{string.Join(", ", _queue)}]";
-    }
+		// Remove and return the item with the highest priority
+		var value = _queue[highPriorityIndex].Value;
+		var nextpersons = new PriorityItem(value, _queue[highPriorityIndex].Priority);
+
+		_queue.RemoveAt(highPriorityIndex);
+		_queue.Add(nextpersons); // Re-add the item to the back of the queue
+		return value;
+	}
+
+	// DO NOT MODIFY THE CODE IN THIS METHOD
+	// The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
+	public override string ToString()
+	{
+		return $"[{string.Join(", ", _queue)}]";
+	}
 }
 
 internal class PriorityItem
 {
-    internal string Value { get; set; }
-    internal int Priority { get; set; }
+	internal string Value { get; set; }
+	internal int Priority { get; set; }
 
-    internal PriorityItem(string value, int priority)
-    {
-        Value = value;
-        Priority = priority;
-    }
+	internal PriorityItem(string value, int priority)
+	{
+		Value = value;
+		Priority = priority;
+	}
 
-    // DO NOT MODIFY THE CODE IN THIS METHOD
-    // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
-    public override string ToString()
-    {
-        return $"{Value} (Pri:{Priority})";
-    }
+	// DO NOT MODIFY THE CODE IN THIS METHOD
+	// The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
+	public override string ToString()
+	{
+		return $"{Value} (Pri:{Priority})";
+	}
 }
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -5,25 +5,67 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 [TestClass]
 public class PriorityQueueTests
 {
-    [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
-    {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
-    }
+	[TestMethod]
+	// Scenario: Dequeueing items with highest priorities and should return them at the back of the queue no matter their priority value is
+	// Expected Result: sonya, sonya, sonya
+	// Defect(s) Found: 
+	public void TestPriorityQueue_1()
+	{
 
-    [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
-    {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
-    }
+		var sophie = new PriorityItem("Sophie", 1);
+		var lance = new PriorityItem("Lance", 10);
+		var sonya = new PriorityItem("Sonya", 31);
 
-    // Add more test cases as needed below.
+		var priorityQueue = new PriorityQueue();
+
+		priorityQueue.Enqueue(sophie.Value, sophie.Priority);
+		priorityQueue.Enqueue(lance.Value, lance.Priority);
+		priorityQueue.Enqueue(sonya.Value, sonya.Priority);
+
+		PriorityItem[] expectedResult = [sonya, sonya, sonya];
+
+		int i = 0;
+		while (i < expectedResult.Length)
+		{
+			var removed = priorityQueue.Dequeue();
+
+			Assert.AreEqual(expectedResult[i].Value, removed);
+			i++;
+		}
+
+		// Assert.Fail("Implement the test case and then remove this.");
+	}
+
+	[TestMethod]
+	// Scenario: Dequeueing items with the same priority and should return at the back of the queue no matter their priority value is
+	// Expected Result: sophie, lance, sophie, lance
+	// Defect(s) Found:
+	public void TestPriorityQueue_2()
+	{
+
+		var sophie = new PriorityItem("Sophie", 10);
+		var lance = new PriorityItem("Lance", 10);
+		var sonya = new PriorityItem("Sonya", 9);
+
+		var priorityQueue = new PriorityQueue();
+
+		priorityQueue.Enqueue(sophie.Value, sophie.Priority);
+		priorityQueue.Enqueue(lance.Value, lance.Priority);
+		priorityQueue.Enqueue(sonya.Value, sonya.Priority);
+
+		PriorityItem[] expectedResult = [sophie, lance, sophie, lance];
+
+		int i = 0;
+		while (i < expectedResult.Length)
+		{
+			var removed = priorityQueue.Dequeue();
+
+			Assert.AreEqual(expectedResult[i].Value, removed);
+			i++;
+		}
+
+		// Assert.Fail("Implement the test case and then remove this.");
+	}
+
+	// Add more test cases as needed below.
 }
